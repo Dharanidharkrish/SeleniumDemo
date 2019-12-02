a113bac04a9d55dc71eec1894268895b1e7a02cf
Dharanidharkrish <dharanidharkrish>
2019-12-02 17:09:22 +0530

    Files are Added

---
+++ src/test/java/guru/test99/AppTest.java
@@ -0,0 +1,37 @@
+package guru.test99;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	protected WebDriver driver;
+	@Test
+  public void test() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+		        "/usr/bin/chromedriver");
+		ChromeOptions opt = new ChromeOptions();
+		opt.setHeadless(true);
+		opt.addArguments("window-size=1920x1080");
+		//opt.addArguments("--no-sandbox");
+		driver = new ChromeDriver(opt);
+		driver.get("https://www.wikipedia.org/");
+		driver.findElement(By.name("search")).sendKeys("Sachin Tendulkar"+ keys.ENTER);
+               
+          String title= driver.getTitle();
+          System.out.println(h);
+        
+          if(h.equalsIgnoreCase("Sachin Tendulkar - Wikipedia")){
+          System.out.println("Success");
+          }
+          else{
+          System.out.println("Failure");
+          }       
+        driver.close();
+		
+	}
+}
+
